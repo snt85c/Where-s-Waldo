278012941f666d2d33d01d5c90ff21c879b62a13
snt85c <santi.cacciola@hotmail.it>
2022-08-06 12:59:26 +0100

    test

--- src/Components/CityBackground.js
+++ src/Components/CityBackground.js
@@ -1,20 +1,24 @@
 import cityBackground from "../img/cityBackground.png";
+import { useState } from "react";
 export default function CityBackground({ setCoordinates, coordinates, ui }) {
   return (
     <img
       scr={cityBackground}
       onTouchStart={(e) => {
-        console.log(e.touches[0].pageX, e.touches[0].pageY);
+    
       }}
       onTouchMove={(e) => {
         if (ui.isGameStart && !ui.isGameOver && !ui.isInstructionOverlayOpen) {
           //the screen wont move if the game is yet to start, won, or the instruction overlay is open, pausing the game
           setCoordinates({
             ...coordinates,
-            screenX: e.touches[0].pageX,
+            screenX: e.touches[0].pageX ,
             screenY: e.touches[0].pageY,
           });
         }
+      }}
+      onTouchEnd={(e)=>{
+  
       }}
   
       onMouseMove={(e) => {
@@ -38,7 +42,7 @@ export default function CityBackground({ setCoordinates, coordinates, ui }) {
         backgroundRepeat: "no-repeat",
         backgroundPosition:
           window.innerWidth < 600
-            ? `${-coordinates.screenX * 2}px ${-coordinates.screenY * 2}px`
+            ? `${-coordinates.screenX * 4}px ${-coordinates.screenY * 2}px`
             : `${-coordinates.screenX * 1.2}px ${-coordinates.screenY * 2}px`,
       }}
       alt="#"
