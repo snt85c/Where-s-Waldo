a9a52b6ceba3082646fe0567b78b03d7b2a8695c
snt85c <santi.cacciola@hotmail.it>
2022-08-06 10:10:48 +0100

    test

--- src/Components/CityBackground.js
+++ src/Components/CityBackground.js
@@ -2,26 +2,31 @@ import cityBackground from "../img/cityBackground.png";
 import { useState } from "react";
 
 export default function CityBackground({ setCoordinates, coordinates, ui }) {
-  const [touchEnd, setTouchEnd] = useState({xEnd:0, yEnd:0})
+  const [touchEnd, setTouchEnd] = useState({ xEnd: 0, yEnd: 0 });
+  console.log(window.innerWidth);
   return (
     <img
       scr={cityBackground}
       onTouchStart={() => {
-        console.log(coordinates.screenX, coordinates.screenY);
+        // console.log(coordinates.screenX, coordinates.screenY);
       }}
       onTouchMove={(e) => {
         if (ui.isGameStart && !ui.isGameOver && !ui.isInstructionOverlayOpen) {
           //the screen wont move if the game is yet to start, won, or the instruction overlay is open, pausing the game
+          // console.log(e.touches[0].pageX, e.touches[0].pageY)
           setCoordinates({
             ...coordinates,
-            screenX: (e.touches[0].pageX + touchEnd.xEnd ) * 2, 
-            screenY: e.touches[0].pageY +  touchEnd.yEnd,
+            screenX: touchEnd.xEnd + e.touches[0].pageX,
+            screenY: touchEnd.yEnd + e.touches[0].pageY,
           });
         }
       }}
       onTouchEnd={(e) => {
-        setTouchEnd({xEnd:e.changedTouches[0].pageX, yEnd:e.changedTouches[0].pageY})
-        console.log(touchEnd)
+        setTouchEnd({
+          xEnd: e.changedTouches[0].pageX,
+          yEnd: e.changedTouches[0].pageY,
+        });
+        // console.log(touchEnd);
       }}
       onMouseMove={(e) => {
         if (ui.isGameStart && !ui.isGameOver && !ui.isInstructionOverlayOpen) {
@@ -42,9 +47,10 @@ export default function CityBackground({ setCoordinates, coordinates, ui }) {
         minHeight: "100vh",
         background: `url(${cityBackground})`,
         backgroundRepeat: "no-repeat",
-        backgroundPosition: `${-coordinates.screenX * 1.2}px ${
-          -coordinates.screenY * 2
-        }px`,
+        backgroundPosition:
+          window.innerWidth < 600
+            ? `${-coordinates.screenX * 3}px ${-coordinates.screenY * 2}px`
+            : `${-coordinates.screenX * 1.2}px ${-coordinates.screenY * 2}px`,
       }}
       alt="#"
     />
